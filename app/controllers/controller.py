a4f1e1c85b4d4eb5dad0f2fb884fd116f2ce625d
Matus Ravas <matustabasco@gmail.com>
2022-10-01 02:07:04 +0200

    search top 100 if query is empty string

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,5 +1,7 @@
-from flask import Response, jsonify, request, render_template_string
-from typing import List
+import itertools
+import logging
+from flask import jsonify, request, render_template_string
+from typing import Dict, List
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -8,6 +10,9 @@ from app.dtos import SearchMatch
 from app.dtos.dtos import SongMetaData
 import re
 
+logger = logging.getLogger(__name__)
+
+# Todo setup logger
 
 BASE_URL = 'https://tabs.ultimate-guitar.com'
 BASE_URL_TAB = 'https://tabs.ultimate-guitar.com/tab/'
@@ -18,59 +23,73 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-def sort_fun(e):
+def sort_fun_tabs(e: Dict):
     if 'votes' in e and 'rating' in e:
         return e['votes'] * e['rating']
+    else:  
+        return -1
+
+
+def sort_fun_hits(e: Dict):
+    if 'hits' in e:
+        return int(e['hits'])
     else:  
         return -1
     
 
 @app.route('/songs')
 def search():
-    # SEARCH_PHRASE = 'viva la vida'
-    # TYPES = {'chords': 300, 'tabs': 200}
-    # TYPE = 300
-    # PAGE = 1
-    
     SEARCH_PHRASE = request.args.get('query', '')
     TYPES = {'chords': 300, 'tabs': 200}
     TYPE = request.args.get('type', 300)
     PAGE = request.args.get('page', 1)
-    SORT = request.args.get('sort', 'desc')
-
-
-    path_search = f'https://www.ultimate-guitar.com/search.php?title={SEARCH_PHRASE}&type={TYPE}&page={PAGE}'
+    SORT = request.args.get('sortOrder', 'desc')
+    ORDER = request.args.get('order', 'rating_desc') # hitstotal_desc rating_desc
+    
+    if SEARCH_PHRASE == '':
+        path_search = f'https://www.ultimate-guitar.com/top/tabs?order={ORDER}&type=chords'
+    else:
+        path_search = f'https://www.ultimate-guitar.com/search.php?title={SEARCH_PHRASE}&type={TYPE}&page={PAGE}'
     page = requests.get(path_search)
 
-    # print(page.content)
     soup = BeautifulSoup(page.text, 'html.parser')
     div = soup.find("div", {'class': 'js-store'})
     s_content = div['data-content']
-    results = json.loads(s_content)['store']['page']['data']['results']
-    results_sorted = sorted(results, key = sort_fun, reverse=True if SORT == 'desc' else False)
+    if SEARCH_PHRASE == '':
+        results = json.loads(s_content)['store']['page']['data']['tabs']
+        hits = json.loads(s_content)['store']['page']['data']['hits']
+        results_sorted = results
+        hits_sorted = sorted(hits, key = sort_fun_hits, reverse=True if SORT == 'desc' else False)
+    else:
+        results = json.loads(s_content)['store']['page']['data']['results']
+        results_sorted = sorted(results, key = sort_fun_tabs, reverse=True if SORT == 'desc' else False)
+        hits_sorted = []
+    
     search_matches: List[SearchMatch] = []
-    for result in results_sorted:
+    for result, hit in itertools.zip_longest(results_sorted, hits_sorted):
+        number_of_hits = hit.get('hits', None) if hit else None
         result_type = result.get('type', None)
         if not result_type: 
             continue
         
         artist_name = result.get('artist_name', 'N/A')
+        artist_id = result.get('artist_id', None)
         song_name = result.get('song_name', 'N/A')
+        song_id = result.get('song_name', None)
         artist_url = result.get('artist_url', 'N/A')
-        tab_url = result.get('tab_url', None)
-        full_link = result.get('tab_url', None)
+        chords_link = result.get('tab_url', None)
+        full_url = result.get('tab_url', None)
         
         rating = result.get('rating', None)
         votes = result.get('votes', None)
-        metadata = SongMetaData(votes, rating)
+        metadata = SongMetaData(votes, rating, number_of_hits)
         
-        if tab_url:
-            tab_url = tab_url.split('.com')[1][5:]
-        else: tab_url = 'N/A'
+        if chords_link:
+            chords_link = chords_link.split('.com')[1][5:]
+        else: chords_link = 'N/A'
         
-        match = SearchMatch(artist_name, song_name, tab_url, full_link, result_type, metadata)
+        match = SearchMatch(artist_name, song_name, chords_link, full_url, result_type, metadata)
         search_matches.append(match)
-        # print(match)
     
     response = jsonify({'ok': True, 'data':[match.serialize() for match in search_matches]})
     response.status_code = 200
@@ -94,7 +113,10 @@ def search_chords():
     s_content = div['data-content']
     result = json.loads(s_content)['store']['page']['data']['tab_view']['wiki_tab']['content']
     
-    body = re.sub(r'(\r\n)+', '<br>', result)
+    body = re.sub(r'(\r\n)+', '<br><br>', result)
+    # body = re.sub(r'(\[ch\])', '', body)
+    # body = re.sub(r'(\[/ch\])', '', body)
+    
     regex = re.compile(r'(\[tab\])|(\[/tab\])', re.I)
     body = regex.sub(r'', body)
     html_page = f'''
@@ -105,6 +127,7 @@ def search_chords():
                 </body>
                 </html>
                 '''
+    return {'ok': True, 'data': html_page} #render_template_string(html_page)
     return render_template_string(html_page)
 
 
--- app/dtos/dtos.py
+++ app/dtos/dtos.py
@@ -1,20 +1,24 @@
-from typing import Dict
+from typing import Dict, Union
 
 class SongMetaData:
-    def __init__(self, votes: int, rating: float) -> None:
+    def __init__(self, votes: int, rating: float, number_of_hits: Union[int, None]) -> None:
         self.votes = votes
         self.rating = rating
+        self.number_of_hits = number_of_hits
         
     def serialize(self) -> Dict:
-        return {'votes': self.votes, 'rating': self.rating, 'score': int(self.votes * self.rating)}
+        obj = {'votes': self.votes, 'rating': self.rating, 'score': int(self.votes * self.rating)}
+        if self.number_of_hits:
+            obj['hits'] = self.number_of_hits
+        return obj
 
 class SearchMatch:
     def __init__(self, artist: str, song_name: str, 
-                 tab_link: str, full_url: str, 
+                 chords_link: str, full_url: str, 
                  result_type: str, song_metadata: SongMetaData) -> None:
         self.artist = artist
         self.song_name = song_name
-        self.tab_link = tab_link
+        self.chords_link = chords_link
         self.full_url = full_url
         self.result_type = result_type
         self.song_metadata = song_metadata
@@ -22,7 +26,7 @@ class SearchMatch:
 
     def serialize(self) -> Dict:
         return {'artist': self.artist, 'song': self.song_name, 
-                'tab_link': self.tab_link, 'full_url': self.full_url, 
+                'chords_link': self.chords_link, 'full_url': self.full_url, 
                 'meta': self.song_metadata.serialize()}
 
 
